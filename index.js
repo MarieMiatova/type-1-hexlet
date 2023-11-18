b4c4b0866f67a26be98ce8f08723eff6e127daea
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2023-11-18 21:22:38 +0000

    index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+function getSum(a, b) {
+  return a + b;
+}
+
+function getSmallestNumber(a, b) {
+  return Math.min(a, b);
+}
+
+function castBoolean(value) {
+  return Boolean(value);
+}
+
+function capitalize(str, num) {
+  if (num < 0 || num >= str.length) {
+    return str;
+  }
+  return str.slice(0, num) + str.charAt(num).toUpperCase() + str.slice(num + 1);
+}
+
+function countCapitalLetters(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+export {
+  getSum,
+  getSmallestNumber,
+  castBoolean,
+  capitalize,
+  countCapitalLetters,
+};
